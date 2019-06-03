522e3f5fcae3ea78ffe410af3c7174979f26b0bc
azdemissiew <ademssiew@gmail.com>
2019-06-03 16:49:27 -0700

    aded the concert by the band function

--- liri.js
+++ liri.js
@@ -59,3 +59,27 @@ function getSpotify(songName){
     });
 };
 liriRun(appCommand,userSearch);
+// creating function for bands
+function getBandsInTown(singer){
+    var singer = userSearch;
+    var singerQueryURL= "https://rest.bandsintown.com/artists/" + singer + "/events?app_id=codingbootcamp";
+axios.get(singerQueryURL).then(
+    function(response){
+        //line breaker in between the bands
+        console.log("====================")
+        //console.log the name of the venue
+        console.log("name of the venue: " + response.data[0].venue.name + "\r\n");
+        //console.log  venue location
+        console.log("venue Location: " + response.data[0].venue.city + "\r\n");
+        // console the date of event
+        console.log("date of event: " + moment(response.data[0].datetime).format("MM-DD-YYYY")+ "\r\n");
+        var concert = "=========the concert by the band=====" + "\nName of the singer: " + singer + "\nName of the venue: " + response.data[0].venue.name + "\r\n"
+        + "venue Location: " + response.data[0].venue.city + "\r\n" + "date of event:"+ moment(response.data[0].datetime).format("MM-DD-YYY") + "\r\n";
+        fs.appendFile("log.txt",concert,function(err){
+            if(err){
+                console.log(err);
+            } 
+
+        });
+    })
+}
